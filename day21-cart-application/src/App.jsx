b5ab50e458729ae3dbf963fab03752f6eecdde31
LakshmiNarasimhan273 <bachelorofphotography@gmail.com>
2025-10-18 20:12:43 +0530

    React Redux Cart Application

---
+++ day21-cart-application/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Routes, Route, Link } from "react-router-dom"
+import Home from "./components/Home"
+import Cart from "./components/Cart"
+
+function App() {
+  return (
+    <div>
+      <nav className='flex justify-between items-center px-8 py-4 bg-white shadow-md'>
+        <h1 className='text-2xl font-bold text-gray-800'>Cartify</h1>
+        <div className='space-x-4'>
+          <Link className='text-blue-600 font-semibold' to="/">Home</Link>
+          <Link className='text-blue-600 font-semibold' to="/cart">My Cart</Link>
+        </div>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ day21-cart-application/src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom"
+import "react-toastify/dist/ReactToastify.css";
+import { Slide, ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
+import { store } from './redux/store.js'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+    <Provider store={store}>
+      <App />
+      <ToastContainer
+      position='bottom-right'
+      autoClose={1500}
+      hideProgressBar={true}
+      theme='dark'
+      transition={Slide}
+      />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>,
+)
