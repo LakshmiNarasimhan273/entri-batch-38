f2408105f99ba184415e9131b896c6ec3f8e2618
LakshmiNarasimhan273 <bachelorofphotography@gmail.com>
2025-11-13 19:38:43 +0530

    Introduction to Backend

---
+++ day22-backend-intro/index.js
@@ -0,0 +1,7 @@
+// import express from "express"; library import in frontend
+const express = require("express"); // library import in backend
+const app = express();
+
+app.listen(8081, () => {
+    console.log('Server running on 8081');
+})
